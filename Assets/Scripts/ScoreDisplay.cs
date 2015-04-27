ad451b368a6a9b53d0c711238933ba5e756a3cd5
nil <Xwszc7@gmail.com>
2015-04-27 18:02:40 +0800

    add the missing score

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+	float timer;
+	// Use this for initialization
+	void Start () {
+		timer = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer < 0)
+			Destroy (gameObject);
+	}
+}
