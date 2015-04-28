e8a332270f5980c04d2bac430dd72776a230f2c4
liu946 <liuyang570926881@gmail.com>
2015-04-28 15:45:03 +0800

    movie player 0.1

---
+++ Assets/movie.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movie : MonoBehaviour {
+
+	// Use this for initialization
+	//电影纹理
+	public MovieTexture movTexture;
+	
+	void Start()
+	{
+		//设置当前对象的主纹理为电影纹理
+		renderer.material.mainTexture = movTexture;
+		//设置电影纹理播放模式为循环
+		movTexture.loop = true;
+	}
+	
+	void OnGUI()
+	{
+		if(GUILayout.Button("播放/继续"))
+		{
+			//播放/继续播放视频
+			if(!movTexture.isPlaying)
+			{
+				movTexture.Play();
+			}
+			
+		}
+		
+		if(GUILayout.Button("暂停播放"))
+		{
+			//暂停播放
+			movTexture.Pause();
+		}
+		
+		if(GUILayout.Button("停止播放"))
+		{
+			//停止播放
+			movTexture.Stop();
+		}
+	}
+
+
+}
+
+
