ca934abcd7a6c58cc206bbce83796763ef273d25
liu946 <liuyang570926881@gmail.com>
2015-04-28 17:00:18 +0800

    movie player

--- Assets/movie.cs
+++ Assets/movie.cs
@@ -10,7 +10,7 @@ public class movie : MonoBehaviour {
 	void Start()
 	{
 		//设置当前对象的主纹理为电影纹理
-		renderer.material.mainTexture = movTexture;
+		GetComponent<Renderer>().material.mainTexture = movTexture;
 		//设置电影纹理播放模式为循环
 		movTexture.loop = true;
 	}
