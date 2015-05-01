7c8590ce8a0337f9b144dc7209096f327344374e
nil <Xwszc7@gmail.com>
2015-05-01 20:05:13 +0800

    make generate controllable

--- Assets/Scripts/GamePlayer.cs
+++ Assets/Scripts/GamePlayer.cs
@@ -8,6 +8,10 @@ public class GamePlayer : MonoBehaviour {
 	NoteGenerator NGRD;
 	NoteGenerator NGLU;
 	NoteGenerator NGLD;
+	ArrayList beatmap;
+	float time;
+	int i;
+
 	// Use this for initialization
 	void Start () {
 		NGDL = GameObject.Find ("NoteGeneratorDL").GetComponent ("NoteGenerator") as NoteGenerator;
@@ -17,13 +21,52 @@ public class GamePlayer : MonoBehaviour {
 		NGLU = GameObject.Find ("NoteGeneratorLU").GetComponent ("NoteGenerator") as NoteGenerator;
 		NGLD = GameObject.Find ("NoteGeneratorLD").GetComponent ("NoteGenerator") as NoteGenerator;
 
+		time = 0f;
+		i = 0;
+
+		/****************
+			// Get beatmap from file
+		****************/
+
+
+
 		// Test
-		NGDL.GenerateNote ();
-		NGLU.GenerateNote ();
+		//NGDL.GenerateNote ();
+		//NGLU.GenerateNote ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		time += Time.deltaTime;	// Timing
 
+		if (0 <= i)	// notes count < i
+			return;
+
+		if (time >= 1) {	// time > generate time
+			switch (1){	// select generator
+			case 1:
+				NGDL.GenerateNote ();
+				break;
+			case 2:
+				NGDR.GenerateNote ();
+				break;
+			case 3:
+				NGLU.GenerateNote ();
+				break;
+			case 4:
+				NGLD.GenerateNote ();
+				break;
+			case 5:
+				NGRU.GenerateNote ();	
+				break;
+			case 6:
+				NGRD.GenerateNote ();	
+				break;
+			default:
+				break;
+			}
+			i++;
+			//now = beatmap [i] as ArrayList;	// move to next note
+		}
 	}
 }
