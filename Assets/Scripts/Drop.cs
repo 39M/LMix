5fd0bcc7ff3f97b2ed1d964e49865277fedcbeef
nil <Xwszc7@gmail.com>
2015-05-18 21:44:17 +0800

    auto miss

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -9,23 +9,23 @@ public class Drop : MonoBehaviour
 	public bool pause;
 	public bool stop;
 	Controller leap;
-	AudioSource audio;
+	AudioSource hitSE;
+	AudioSource missSE;
 	GamePlayer status;
 	Vector FingerPos;
-	float lim_x_low, lim_x_high, lim_y_low, lim_y_high, lim_z_low, lim_z_high;
 	Vector3 notePos;
+	float lim_x_low, lim_x_high, lim_y_low, lim_y_high, lim_z_low, lim_z_high;
 	bool hit;
 
-	// Use this for initialization
 	void Start ()
 	{
 		leap = new Controller ();
 		hit = pause = stop = false;
-		audio = GetComponent<AudioSource> ();
+		hitSE = GetComponent<AudioSource> ();
+		missSE = hitSE;
 		status = GameObject.Find ("GamePlayer").GetComponent ("GamePlayer") as GamePlayer;
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
 		if (status.stop)
@@ -33,11 +33,15 @@ public class Drop : MonoBehaviour
 		if (status.pause)
 			return;
 
+		if ((notePos.z <= -2 && !missSE.isPlaying) || (hit && !hitSE.isPlaying)) {
+			Destroy (gameObject);
+		}
+
 		transform.Translate (new Vector3 (10, 0, 0) * Time.deltaTime * speed);
 		notePos = transform.position;
-		if (!hit && notePos.z < 1.75 && notePos.z > -1.75) {
+		if (!hit && notePos.z < 2 && notePos.z > -2) {
 			lim_z_low = -175;
-			lim_z_high = 200;
+			lim_z_high = 175;
 			if (notePos.x > 0) {
 				if (notePos.y < -0.5) {
 					lim_x_low = 0;
@@ -114,7 +118,7 @@ public class Drop : MonoBehaviour
 						status.ComboText.text = "Combo: " + status.ComboCounter.ToString ();
 
 						hit = true;
-						audio.Play ();
+						hitSE.Play ();
 						GetComponent<Renderer> ().enabled = false;
 						//Destroy (gameObject);
 						return;
@@ -123,8 +127,21 @@ public class Drop : MonoBehaviour
 			}
 		}
 
-		if (notePos.z < -10 || (hit && !audio.isPlaying))
-			Destroy (gameObject);
+		if (notePos.z <= -2 && !hit && !missSE.isPlaying) {
+			Quaternion rt = Quaternion.identity;
+			if (transform.rotation.eulerAngles.x == 0)
+				rt.eulerAngles = new Vector3 (0, -90, 0);
+			else if (transform.rotation.eulerAngles.x == 90)
+				rt.eulerAngles = new Vector3 (0, 0, 0);
+			else// if (transform.rotation.eulerAngles.x == 180)
+				rt.eulerAngles = new Vector3 (0, 90, 0);
+
+			GameObject tmp = (GameObject)Instantiate (score, transform.position, rt);
+			tmp.GetComponent<TextMesh> ().text = "Miss!";
+			status.ComboCounter = 0;
+			GetComponent<Renderer> ().enabled = false;
+			missSE.Play ();
+		}
 	}
 }
 
