d44bd556cc5ba24542b09587681de93d82f71a33
liu946 <liuyang570926881@gmail.com>
2015-05-23 18:44:39 +0800

    select background

---
+++ Assets/Scenes/SelectMusic/Scripts/backmovie.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backmovie : MonoBehaviour {
+
+	// Use this for initialization
+
+	public MovieTexture movTexture;
+	
+	void Start()
+	{
+		//设置当前对象的主纹理为电影纹理
+		GetComponent<Renderer>().material.mainTexture  = movTexture;
+		//设置电影纹理播放模式为循环
+		movTexture.loop = true;
+		if(!movTexture.isPlaying)
+		{
+			movTexture.Play();
+		}
+	}
+}
