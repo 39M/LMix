da0d0b176dcf8bdcfe65852298e24ce072888e70
liu946 <liuyang570926881@gmail.com>
2015-05-27 18:22:57 +0800

    difficult selected added

--- Assets/Scenes/SelectMusic/Scripts/MenuController.cs
+++ Assets/Scenes/SelectMusic/Scripts/MenuController.cs
@@ -7,17 +7,25 @@ using SimpleJSON;
 public class MenuController : MonoBehaviour {
 	public GameObject menuitemobj;
 	public List<GameObject> menulist= new List<GameObject> ();
-	public int musicnum;
+	public int musicnum = 0;
 	protected bool motionlock=false;
 	protected bool selectlock = false;
 	protected double lastx;
 	protected Controller leap;
 	protected Vector3 lastpos;
+	protected Dictionary<string,int> diff ;
+	protected Dictionary<string,string> song ;
 	// Use this for initialization
 	void Start () {
+		// init 
+		this.diff = new Dictionary<string, int>();
+		this.song = new Dictionary<string,string>();
+		leap = new Controller ();
+
 		// read dir from resource music
 		string[] files = Directory.GetDirectories (@"Assets/Resources/Music/");
 		List<string> a = new List<string> ();
+	
 		foreach (var item in files) {
 			string folder = Path.GetFileName(item);
 			Debug.Log(" Read Music Dir " + folder + " and load beatmap.");
@@ -27,25 +35,28 @@ public class MenuController : MonoBehaviour {
 				Debug.Log ("Beatmap for "+ folder+" load failed !!!");
 				return;
 			}
-			Debug.Log ("Beatmap for "+ folder+"load success");
-			var Beatmap = JSON.Parse (f.ToString ());
+			Debug.Log("Beatmap for "+ folder+"load success");
+			JSONNode Beatmap = JSON.Parse(f.ToString());
 
+			List<string> diffcollection = new List<string>(){"Easy","Normal","Hard"};
+			Dictionary<string,int> diffmap = new Dictionary<string, int>(){{"Easy",0},{"Normal",1},{"Hard",2}}; 
+			foreach(string difficulty in diffcollection){
+				if(Beatmap["Difficulty"][difficulty]["Enable"].AsBool){
+					Vector3 pos=new Vector3(transform.position.x,transform.position.y,transform.position.z);
+					pos.x += (this.musicnum*0.5f);
+					GameObject menuitem_tmp = (GameObject) Instantiate(menuitemobj,pos,transform.rotation);
+					Debug.Log("loading "+"Music/"+folder+Beatmap["Album"]["Name"]);
+					menuitem_tmp.GetComponent<GUITexture>().texture=(Texture)Resources.Load("Music/"+folder+"/"+Beatmap["Album"]["Name"]);
+					menuitem_tmp.GetComponent<GUIText>().text=Beatmap["Title"]+difficulty;
+					menulist.Add(menuitem_tmp);
+					diff[Beatmap["Title"]+difficulty]=diffmap[difficulty];
+					song[Beatmap["Title"]+difficulty]=folder;
+					this.musicnum ++ ;
+				}
+			}
 		}
 		//
-		leap = new Controller ();
-
-		var i = 0;
-		foreach(var item in a){
-			Vector3 pos=new Vector3(transform.position.x,transform.position.y,transform.position.z);
-			pos.x += (i*0.5f);
-			GameObject menuitem_tmp = (GameObject) Instantiate(menuitemobj,pos,transform.rotation);
-			menuitem_tmp.GetComponent<GUITexture>().texture=(Texture)Resources.Load(item);//AssetBundle.CreateFromFile("tmpfile/"+item+".jpg");
-			menuitem_tmp.GetComponent<GUIText>().text=item;
-			menulist.Add(menuitem_tmp);
-			i++;
-		}
-		musicnum = i;
-
+	
 	}
 	
 	// Update is called once per frame
@@ -94,8 +105,14 @@ public class MenuController : MonoBehaviour {
 						}
 						if(item.transform.position.y>0.8){
 							// select success
-							Debug.Log("enter "+item.GetComponent<GUIText>().text);
-							PlayerPrefs.SetString("song",item.GetComponent<GUIText>().text);
+							Debug.Log("enter "+this.song[item.GetComponent<GUIText>().text]);
+							PlayerPrefs.SetString("song",this.song[item.GetComponent<GUIText>().text]);
+							//enableSE = true;
+							//enableBG = true;
+							PlayerPrefs.SetInt("enableSE",1);
+							PlayerPrefs.SetInt("enableBG",1);
+							Debug.Log("set difficulty "+ this.diff[item.GetComponent<GUIText>().text]);
+							PlayerPrefs.SetInt("difficulty",this.diff[item.GetComponent<GUIText>().text]);
 							Application.LoadLevel("InGame");
 						}
 					}
