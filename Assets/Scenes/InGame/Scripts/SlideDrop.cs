d5c9d822aa622ae9c691d7c99fd9f1bb9b024ee4
nil <Xwszc7@gmail.com>
2015-06-10 14:16:08 +0800

    use tap detector to make tap judgement

--- Assets/Scenes/InGame/Scripts/SlideDrop.cs
+++ Assets/Scenes/InGame/Scripts/SlideDrop.cs
@@ -8,23 +8,26 @@ public class SlideDrop : MonoBehaviour
 	public GameObject judgement;
 	public bool pause;
 	public bool stop;
-	Controller leap;
+	//Controller leap;
 	AudioSource hitSE;
 	AudioSource missSE;
 	GamePlayer status;
+	TapDetector TD;
 	Vector FingerPos;
 	Vector3 notePos;
-	float lim_x_low, lim_x_high, lim_y_low, lim_y_high, lim_z_low, lim_z_high;
+	//float lim_x_low, lim_x_high, lim_y_low, lim_y_high, lim_z_low, lim_z_high;
 	bool hit;
+	int track;
 	//bool miss;
 	Color c300, c100, c50, c0;
 
 	void Start ()
 	{
-		leap = new Controller ();
+		//leap = new Controller ();
 		hit = pause = stop = false;
 
 		status = GameObject.Find ("GamePlayer").GetComponent ("GamePlayer") as GamePlayer;
+		TD = GameObject.Find ("TapDetector").GetComponent ("TapDetector") as TapDetector;
 
 		hitSE = GetComponent<AudioSource> ();
 		if (!status.defaultSE)
@@ -40,46 +43,50 @@ public class SlideDrop : MonoBehaviour
 		c50 = new Color (251 / 255f, 208 / 255f, 114 / 255f, 0);
 		c0 = new Color (249 / 255f, 90 / 255f, 101 / 255f, 0);
 
-		int width = 200, y_base = 0;
+		//int width = 200, y_base = 0;
 		float y_basef = 1.15f;
-		lim_z_low = -175 * speed / 1.4f;
-		lim_z_high = 175 * speed / 1.4f;
+		//lim_z_low = -175 * speed / 1.4f;
+		//lim_z_high = 175 * speed / 1.4f;
 		
 		notePos = transform.position;
 		if (notePos.x > 0) {
 			if (notePos.y < y_basef - 0.5) {
-				lim_x_low = 0;
+				track = 0;
+				/*lim_x_low = 0;
 				lim_x_high = 200;
 				lim_y_low = y_base - 80;
-				lim_y_high = lim_y_low + width;	// 80
+				lim_y_high = lim_y_low + width;	// 80*/
 			} else if (notePos.y < y_basef + 2) {
-				lim_x_low = 170;
+				track = 2;
+				/*lim_x_low = 170;
 				lim_x_high = 300;
 				lim_y_low = y_base + 50;
-				lim_y_high = lim_y_low + width;
-			} else {
+				lim_y_high = lim_y_low + width;*/
+			} /*else {
 				lim_x_low = 170;
 				lim_x_high = 300;
 				lim_y_low = 250;
 				lim_y_high = 450;
-			}
+			}*/
 		} else {
 			if (notePos.y < y_basef - 0.5) {
-				lim_x_low = -200;
+				track = 1;
+				/*lim_x_low = -200;
 				lim_x_high = 0;
 				lim_y_low = y_base - 80;
-				lim_y_high = lim_y_low + width;	// 80
+				lim_y_high = lim_y_low + width;	// 80*/
 			} else if (notePos.y < y_basef + 2) {
-				lim_x_low = -300;
+				track = 3;
+				/*lim_x_low = -300;
 				lim_x_high = -170;
 				lim_y_low = y_base + 50;
-				lim_y_high = lim_y_low + width;
-			} else {
+				lim_y_high = lim_y_low + width;*/
+			} /*else {
 				lim_x_low = -300;
 				lim_x_high = -170;
 				lim_y_low = 250;
 				lim_y_high = 450;
-			}
+			}*/
 		}
 	}
 
@@ -135,67 +142,85 @@ public class SlideDrop : MonoBehaviour
 				}
 			}*/
 
-			Frame frame = leap.Frame ();
-			foreach (Hand hand in frame.Hands) {
-				// Debug.Log (hand.Fingers.Frontmost.TipPosition);
-				foreach (Finger finger in hand.Fingers) {
-					FingerPos = finger.TipPosition;
-					// Destroy note if tap success
-					if (lim_y_low < FingerPos.y && FingerPos.y < lim_y_high && 
-						lim_x_low < FingerPos.x && FingerPos.x < lim_x_high && 
-						lim_z_low < FingerPos.z && FingerPos.z < lim_z_high) {
-
-						Quaternion rt = Quaternion.identity;
-						if (transform.rotation.eulerAngles.x == 0)
-							rt.eulerAngles = new Vector3 (0, -90, 0);
-						else if (transform.rotation.eulerAngles.x == 90)
-							rt.eulerAngles = new Vector3 (0, 0, 0);
-						else// if (transform.rotation.eulerAngles.x == 180)
-							rt.eulerAngles = new Vector3 (0, 90, 0);
-						//Debug.Log(transform.rotation.eulerAngles.x / 2);
-						TextMesh tmp = ((GameObject)Instantiate (judgement, transform.position, rt)).GetComponent<TextMesh> ();
-						int ScoreGet;
-						if (notePos.z > -0.75 * speed / 1.4) {
-							tmp.text = "Perfect";
-							tmp.color = c300;
-							ScoreGet = 300 + 300 / 25 * status.ComboCounter;
-							status.ComboCounter++;
-							status.PerfectCount++;
-						} else if (notePos.z > -1.5 * speed / 1.4) {
-							tmp.text = "Good";
-							tmp.color = c100;
-							status.ComboCounter++;
-							status.GoodCount++;
-							ScoreGet = 100 + 100 / 25 * status.ComboCounter;
-						} else if (notePos.z > -1.75 * speed / 1.4) {
-							tmp.text = "Bad";
-							tmp.color = c50;
-							ScoreGet = 50 + 50 / 25 * status.ComboCounter;
-							status.ComboCounter = 0;
-							status.BadCount++;
-						} else {
-							tmp.text = "Miss";
-							tmp.color = c0;
-							status.MissCount++;
-							ScoreGet = 0;
-						}
-						status.ScoreCounter += ScoreGet;
-						if (status.ComboCounter > status.MaxCombo)
-							status.MaxCombo = status.ComboCounter;
-						//status.ScoreText.text = "Score: " + status.ScoreNow.ToString ();
-						status.ComboText.text = "Combo: " + status.ComboCounter.ToString ();
-
-						hit = true;
-						if (status.enableSE) {
-							hitSE.enabled = true;
-							hitSE.Play ();
-						}
-						GetComponent<Renderer> ().enabled = false;
-						//Destroy (gameObject);
-						return;
-					}
-				}
+			bool tap_success = false;
+			switch (track) {
+			case 0:	// DR
+				tap_success = TD.DownRight;
+				break;
+			case 1:	// DL	
+				tap_success = TD.DownLeft;
+				break;
+			case 2:	// R
+				tap_success = TD.Right;
+				break;
+			case 3:	// L
+				tap_success = TD.Left;
+				break;
 			}
+
+			//Frame frame = leap.Frame ();
+			//foreach (Hand hand in frame.Hands) {
+			// Debug.Log (hand.Fingers.Frontmost.TipPosition);
+			//foreach (Finger finger in hand.Fingers) {
+			//FingerPos = finger.TipPosition;
+			// Destroy note if tap success
+			/*if (lim_y_low < FingerPos.y && FingerPos.y < lim_y_high && 
+						lim_x_low < FingerPos.x && FingerPos.x < lim_x_high && 
+						lim_z_low < FingerPos.z && FingerPos.z < lim_z_high) {*/
+
+
+			if (tap_success) {
+				Quaternion rt = Quaternion.identity;
+				if (transform.rotation.eulerAngles.x == 0)
+					rt.eulerAngles = new Vector3 (0, -90, 0);
+				else if (transform.rotation.eulerAngles.x == 90)
+					rt.eulerAngles = new Vector3 (0, 0, 0);
+				else// if (transform.rotation.eulerAngles.x == 180)
+					rt.eulerAngles = new Vector3 (0, 90, 0);
+				//Debug.Log(transform.rotation.eulerAngles.x / 2);
+				TextMesh tmp = ((GameObject)Instantiate (judgement, transform.position, rt)).GetComponent<TextMesh> ();
+				int ScoreGet;
+				if (notePos.z > -0.75 * speed / 1.4) {
+					tmp.text = "Perfect";
+					tmp.color = c300;
+					ScoreGet = 300 + 300 / 25 * status.ComboCounter;
+					status.ComboCounter++;
+					status.PerfectCount++;
+				} else if (notePos.z > -1.5 * speed / 1.4) {
+					tmp.text = "Good";
+					tmp.color = c100;
+					status.ComboCounter++;
+					status.GoodCount++;
+					ScoreGet = 100 + 100 / 25 * status.ComboCounter;
+				} else if (notePos.z > -1.75 * speed / 1.4) {
+					tmp.text = "Bad";
+					tmp.color = c50;
+					ScoreGet = 50 + 50 / 25 * status.ComboCounter;
+					status.ComboCounter = 0;
+					status.BadCount++;
+				} else {
+					tmp.text = "Miss";
+					tmp.color = c0;
+					status.MissCount++;
+					ScoreGet = 0;
+				}
+				status.ScoreCounter += ScoreGet;
+				if (status.ComboCounter > status.MaxCombo)
+					status.MaxCombo = status.ComboCounter;
+				//status.ScoreText.text = "Score: " + status.ScoreNow.ToString ();
+				status.ComboText.text = "Combo: " + status.ComboCounter.ToString ();
+
+				hit = true;
+				if (status.enableSE) {
+					hitSE.enabled = true;
+					hitSE.Play ();
+				}
+				GetComponent<Renderer> ().enabled = false;
+				//Destroy (gameObject);
+				return;
+			}
+			//}
+			//}
 		}
 
 		if (notePos.z <= -1.75 * speed / 1.4 && !hit && !missSE.isPlaying) {
