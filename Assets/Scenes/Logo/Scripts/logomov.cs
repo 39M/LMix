63561eec32ffbec37274aa3a382de7543250ebd8
liu946 <liuyang570926881@gmail.com>
2015-06-19 00:29:05 +0800

    Logo Show New Scene

---
+++ Assets/Scenes/Logo/Scripts/logomov.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logomov : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	// Use this for initialization
+	void Start () {
+		movTexture = Resources.Load ("logo") as MovieTexture;
+		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		
+		movTexture.loop = true;
+		if (!movTexture.isPlaying) {
+			movTexture.Play ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
