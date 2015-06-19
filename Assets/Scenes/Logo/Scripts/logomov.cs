6ee1ff1f6b58a135a5fa835e2daea87ce375f692
liu946 <liuyang570926881@gmail.com>
2015-06-19 19:16:01 +0800

    logo load logowav

--- Assets/Scenes/Logo/Scripts/logomov.cs
+++ Assets/Scenes/Logo/Scripts/logomov.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class logomov : MonoBehaviour {
 
 	public MovieTexture movTexture;
+	public AudioClip audioclip;
 
 	// Use this for initialization
 	void Start () {
-		movTexture = Resources.Load ("logo") as MovieTexture;
+		movTexture = Resources.Load ("logomov") as MovieTexture;
+		audioclip = Resources.Load ("logowav") as AudioClip;
 		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		GetComponent<AudioSource> ().clip = audioclip;
 
 		movTexture.loop = false;
 		if (!movTexture.isPlaying) {
 			movTexture.Play ();
+			GetComponent<AudioSource> ().Play();	
 		}
 	}
 	
