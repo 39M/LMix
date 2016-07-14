47a282702ea3bb4051696e26717d888abd97a200
Jiaqi Wang <xwszc7@gmail.com>
2016-07-14 17:48:50 +0800

    vr adjust

--- Assets/Scenes/SelectMusic/Scripts/MenuController.cs
+++ Assets/Scenes/SelectMusic/Scripts/MenuController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -8,6 +9,7 @@ using SimpleJSON;
 public class MenuController : MonoBehaviour
 {
 	public GameObject menuitemobj;
+    public GameObject canvas;
 	public List<GameObject> menulist = new List<GameObject> ();
 	public int musicnum = 0;
 	protected bool motionlock = false;
@@ -34,12 +36,16 @@ public class MenuController : MonoBehaviour
 
 	protected bool QuitGameFlag =false;
 
-	GUIText difftextobj;
+    float spaceHorizontal = 5.63f;
+    int curID;
+
+    Text difftextobj;
 
 //record the back needed
 	// Use this for initialization
 	void Start ()
 	{
+        //PlayerPrefs.DeleteAll();
 		difflist = new Dictionary<string, List<string>> ();
 		CoverTexture = GameObject.Find ("Cover").GetComponent<GUITexture> ();
 		// init 
@@ -80,38 +86,51 @@ public class MenuController : MonoBehaviour
 					difflist [Beatmap ["Title"]].Add (difficulty);
 				}
 			}
-			Vector3 pos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-			this.backward [Beatmap ["Title"]] = (this.musicnum * 0.5f);
+            //Vector3 pos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+            Vector3 pos = new Vector3(0, 0, canvas.transform.position.z);
+            this.backward [Beatmap ["Title"]] = (this.musicnum * spaceHorizontal);
 			if (PlayerPrefs.HasKey ("BACKWARD")) {
 				Debug.Log ("has backword " + PlayerPrefs.GetFloat ("BACKWARD"));
 				pos.x -= PlayerPrefs.GetFloat ("BACKWARD");
 			}
 
-			pos.x += (this.musicnum * 0.5f);
-			GameObject menuitem_tmp = (GameObject)Instantiate (menuitemobj, pos, transform.rotation);
-			Debug.Log ("loading " + "Music/" + folder + Beatmap ["Album"] ["Name"]);
-			menuitem_tmp.GetComponent<GUITexture> ().texture = (Texture)Resources.Load ("Music/" + folder + "/" + Beatmap ["Album"] ["Name"]);
-			//Debug.Log (menuitem_tmp.GetComponent<GUITexture> ().border.ToString ());
-			menuitem_tmp.GetComponent<GUIText> ().text = Beatmap ["Title"];// +'\n'+ difficulty;
-			Vector2 tpos = new Vector2 (0, - UnityEngine.Screen.height / 3.5f);
-			menuitem_tmp.GetComponent<GUIText> ().pixelOffset = tpos;
-			menuitem_tmp.GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 15.0f);
+			pos.x += (this.musicnum * spaceHorizontal);
+            //GameObject menuitem_tmp = (GameObject)Instantiate (menuitemobj, pos, transform.rotation);
+            //Debug.Log ("loading " + "Music/" + folder + Beatmap ["Album"] ["Name"]);
+            //menuitem_tmp.GetComponent<GUITexture> ().texture = (Texture)Resources.Load ("Music/" + folder + "/" + Beatmap ["Album"] ["Name"]);
+            ////Debug.Log (menuitem_tmp.GetComponent<GUITexture> ().border.ToString ());
+            //menuitem_tmp.GetComponent<GUIText> ().text = Beatmap ["Title"];// +'\n'+ difficulty;
+            //Vector2 tpos = new Vector2 (0, - UnityEngine.Screen.height / 3.5f);
+            //menuitem_tmp.GetComponent<GUIText> ().pixelOffset = tpos;
+            //menuitem_tmp.GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 15.0f);
 
-			// solve the child GUIText OBJ
+            //// solve the child GUIText OBJ
 
-			menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().text = difflist [Beatmap ["Title"]] [0];
-			menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().color = diffcolormap [difflist [Beatmap ["Title"]] [0]];
-			tpos.y += ((tpos.y) / -83.5f);
-			menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().pixelOffset = tpos;
-			menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 15.0f);
+            //menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().text = difflist [Beatmap ["Title"]] [0];
+            //menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().color = diffcolormap [difflist [Beatmap ["Title"]] [0]];
+            //tpos.y += ((tpos.y) / -83.5f);
+            //menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().pixelOffset = tpos;
+            //menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 15.0f);
 
-			menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().text = Beatmap ["Artist"];
-			menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 10.0f);
-			tpos.y -= ((tpos.y) / -83.5f * 20.0f);
-			menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().pixelOffset = tpos;
+            //menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().text = Beatmap ["Artist"];
+            //menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().fontSize = (int)((tpos.y) / -83.5f * 10.0f);
+            //tpos.y -= ((tpos.y) / -83.5f * 20.0f);
+            //menuitem_tmp.transform.GetChild (1).GetComponent<GUIText> ().pixelOffset = tpos;
 
-			// save the Songs identity
-			menulist.Add (menuitem_tmp);
+            GameObject menuitem_tmp = (GameObject)Instantiate(menuitemobj, pos, transform.rotation);
+            Debug.Log(pos);
+            Debug.Log("loading " + "Music/" + folder + Beatmap["Album"]["Name"]);
+            menuitem_tmp.transform.SetParent(canvas.transform);
+            menuitem_tmp.GetComponent<UnityEngine.UI.Image>().sprite = Resources.Load<Sprite>("Music/" + folder + "/" + Beatmap["Album"]["Name"]);
+            menuitem_tmp.transform.GetChild(0).GetComponent<Text>().text = Beatmap["Title"];
+            menuitem_tmp.transform.GetChild(1).GetComponent<Text>().text = difflist[Beatmap["Title"]][0];
+            menuitem_tmp.transform.GetChild(1).GetComponent<Text>().color = diffcolormap[difflist[Beatmap["Title"]][0]];
+            menuitem_tmp.transform.GetChild(2).GetComponent<Text>().text = Beatmap["Artist"];
+
+
+
+            // save the Songs identity
+            menulist.Add (menuitem_tmp);
 
 			song [Beatmap ["Title"]] = folder;
 			this.songname [Beatmap ["Title"]] = Beatmap ["Audio"] ["Name"];
@@ -120,7 +139,7 @@ public class MenuController : MonoBehaviour
 			this.musicnum ++;
 		
 			Debug.Log ("location " + pos.x);
-			if (pos.x < 0.6 && pos.x > 0.4) {
+			if (pos.x < spaceHorizontal * 0.5f && pos.x > -spaceHorizontal * 0.5f) {
 				// play this item music
 				string songpath = this.song [Beatmap ["Title"]];
 				string songname = this.songname [Beatmap ["Title"]];
@@ -135,12 +154,14 @@ public class MenuController : MonoBehaviour
 				//
 				if (PlayerPrefs.HasKey ("Difficulty")) {
 					Debug.Log ("Has Selected Difficulty!");
-					menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().text = diffcollection [PlayerPrefs.GetInt ("Difficulty")];
-					menuitem_tmp.transform.GetChild (0).GetComponent<GUIText> ().color = diffcolormap [diffcollection [PlayerPrefs.GetInt ("Difficulty")]];
+					menuitem_tmp.transform.GetChild (1).GetComponent<Text> ().text = diffcollection [PlayerPrefs.GetInt ("Difficulty")];
+					menuitem_tmp.transform.GetChild (1).GetComponent<Text> ().color = diffcolormap [diffcollection [PlayerPrefs.GetInt ("Difficulty")]];
 				}
 			}
 
 		}
+        curID = (int) Mathf.Abs(menulist[0].transform.position.x / spaceHorizontal);
+
 		//open the guesture
 		leap.EnableGesture (Gesture.GestureType.TYPE_SWIPE);
 		//leap.Config.SetFloat("Gesture.Swipe.MinLength", 100.0f);
@@ -151,8 +172,10 @@ public class MenuController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		//the cover	
-		if (covermotion) {
+        Debug.Log("Last Motion = " + lastmotion);
+        Debug.Log("Motion lock = " + motionlock);
+        //the cover	
+        if (covermotion) {
 			Color tcolor = CoverTexture.color;
 			float a = tcolor.a + Time.deltaTime * 2 * alphadirection;
 			if (a < 0.0f) {
@@ -204,61 +227,70 @@ public class MenuController : MonoBehaviour
 
 			} else if (lastmotion == -1||lastmotion == 1) {
 
+                int i = 0;
 				foreach (var item in menulist) {
 					// move items and test move finished
 					Vector3 position = item.transform.position;
-					double lx = position.x;
-					position.x += 0.5f * lastmotion * Time.deltaTime;
+                    float lx = position.x;
+                    //position.x += spaceHorizontal * lastmotion * Time.deltaTime;
+                    float targetX;
+                    if (lastmotion == 1)
+                        targetX = (i - curID) * spaceHorizontal + spaceHorizontal;
+                    else
+                        targetX = (i - curID) * spaceHorizontal - spaceHorizontal;
+                    position.x = Mathf.SmoothStep(position.x, targetX, 0.15f);
 					item.transform.position = position;
-					// if any item cross the 0.5f , release the lock;
-					if ((position.x - 0.5) * (lx - 0.5) < 0) {
-						if (playingsong != item.GetComponent<GUIText> ().text) {
-							// play this item music
-							foreach (var in_item in menulist) {
+                    // if any item cross the 0.5f , release the lock;
+                    if ((position.x + 1e-2 * lastmotion) * (lx) < 0)
+                    {
+                        //if (Mathf.Abs(position.x - targetX) < 1e-2) {
+                            if (playingsong != item.transform.GetChild(0).GetComponent<Text> ().text) {
+
+                            // play this item music
+                            int j = 0;
+                            foreach (var in_item in menulist) {
 								Vector3 in_postion = in_item.transform.position;
-								in_postion.x -= 0.5f * lastmotion * Time.deltaTime;
-								in_item.transform.position = in_postion;
+								in_postion.x = (j - curID) * spaceHorizontal + lastmotion * spaceHorizontal;
+                                in_item.transform.position = in_postion;
+                                j++;
 							}
 
-							string folder = this.song [item.GetComponent<GUIText> ().text];
-							string name = this.songname [item.GetComponent<GUIText> ().text];
-							var music = GetComponent<AudioSource> ();
-							music.clip = Resources.Load ("Music/" + folder + "/" + name) as AudioClip;
-							Debug.Log ("paly music demo " + "/Music/" + folder + "/" + name);
-							music.time = this.songstart [item.GetComponent<GUIText> ().text];
-							music.loop = true;
-							music.Play ();
-							playingsong = item.GetComponent<GUIText> ().text;
+							string folder = this.song [item.transform.GetChild(0).GetComponent<Text> ().text];
+							string name = this.songname [item.transform.GetChild(0).GetComponent<Text> ().text];
+                            StartCoroutine(LoadMusic(item, folder, name));
+							playingsong = item.transform.GetChild(0).GetComponent<Text> ().text;
 						}
 						// unlock 
 						this.motionlock = false;
 					}
+                    i++;
 				}
 				GetComponent<AudioSource> ().volume -= Time.deltaTime * 1.2f;
 			} else if (lastmotion == 0){
 				foreach (var item in menulist) {
 					Vector3 position = item.transform.position;
-					if (position.x > 0.4 && position.x < 0.6) {
+					if (position.x > -spaceHorizontal * 0.5f && position.x < spaceHorizontal * 0.5f) {
 
 						// select movement
 
-						position.y += 0.6f * Time.deltaTime;
+						//position.y += 2f * Time.deltaTime;
+                        position.y = Mathf.SmoothStep(position.y, 1f, 0.1f);
 						item.transform.position = position;
 					}
-					if (item.transform.position.y > 0.8) {  
+					if (item.transform.position.y > 0.5f) {  
 
 						// enter the InGame Scene
 
 						// select success
-						Debug.Log ("enter " + this.song [item.GetComponent<GUIText> ().text]);
-						PlayerPrefs.SetString ("song", this.song [item.GetComponent<GUIText> ().text]);
+						Debug.Log ("enter " + this.song [item.transform.GetChild(0).GetComponent<Text> ().text]);
+						PlayerPrefs.SetString ("song", this.song [item.transform.GetChild(0).GetComponent<Text> ().text]);
 						//enableSE = true;
 						//enableBG = true;
 						PlayerPrefs.SetInt ("enableSE", 1);
 						PlayerPrefs.SetInt ("enableBG", 1);
-						PlayerPrefs.SetFloat ("BACKWARD", this.backward [item.GetComponent<GUIText> ().text]);
-						Debug.Log ("set difficulty " + this.diff [item.transform.GetChild (0).GetComponent<GUIText> ().text]);
-						PlayerPrefs.SetInt ("Difficulty", this.diff [item.transform.GetChild (0).GetComponent<GUIText> ().text]);
+						PlayerPrefs.SetFloat ("BACKWARD", this.backward [item.transform.GetChild(0).GetComponent<Text> ().text]);
+						Debug.Log ("set difficulty " + this.diff [item.transform.GetChild (1).GetComponent<Text> ().text]);
+						PlayerPrefs.SetInt ("Difficulty", this.diff [item.transform.GetChild (1).GetComponent<Text> ().text]);
 						Application.LoadLevel ("InGame");
 					}
 
@@ -266,8 +298,11 @@ public class MenuController : MonoBehaviour
 			}
 
 		} else {
-			// fix the low sound
-			if (GetComponent<AudioSource> ().volume < 1.0) {
+            curID = Mathf.RoundToInt(Mathf.Abs(menulist[0].transform.position.x / spaceHorizontal));
+            Debug.Log("Cur ID = " + curID);
+
+            // fix the low sound
+            if (GetComponent<AudioSource> ().volume < 1.0) {
 				GetComponent<AudioSource> ().volume += Time.deltaTime * 0.5f;
 			}
 			// keyboard
@@ -281,13 +316,14 @@ public class MenuController : MonoBehaviour
 				motionlock = true;
 			}
 			if (Input.GetKey (KeyCode.A)||Input.GetKey (KeyCode.LeftArrow)) {
-				if(menulist [0].transform.position.x < 0.4f){
+				if(menulist [0].transform.position.x < -spaceHorizontal * 0.5f){
 					lastmotion = 1;
 					motionlock = true;
 				}
 			}
 			if (Input.GetKey (KeyCode.D)||Input.GetKey (KeyCode.RightArrow)) {
-				if((menulist [0].transform.position.x + (menulist.Count - 1) * 0.5) > 0.6f){
+				if((menulist [0].transform.position.x + (menulist.Count - 1) * spaceHorizontal) > spaceHorizontal * 0.5f)
+                {
 					lastmotion = -1;
 					motionlock = true;
 				}
@@ -297,14 +333,14 @@ public class MenuController : MonoBehaviour
 				motionlock = true;
 				foreach (var item in menulist) {
 					Vector3 position = item.transform.position;
-					if (position.x > 0.4 && position.x < 0.6) {
-						var list = difflist [item.GetComponent<GUIText> ().text];
-						int index = list.IndexOf (item.transform.GetChild (0).GetComponent<GUIText> ().text);
+					if (position.x > -spaceHorizontal * 0.5f && position.x < spaceHorizontal * 0.5f) {
+						var list = difflist [item.transform.GetChild(0).GetComponent<Text> ().text];
+						int index = list.IndexOf (item.transform.GetChild (1).GetComponent<Text> ().text);
 						Debug.Log ("diff content list " + list.ToString () + " index: " + index);
 						
 						index = (index + 1) % list.Count;
 						difftonext = list [index];
-						difftextobj = item.transform.GetChild (0).GetComponent<GUIText> ();
+						difftextobj = item.transform.GetChild (1).GetComponent<Text> ();
 						//item.transform.GetChild(0).GetComponent<GUIText>().text = list[index];
 						//item.transform.GetChild(0).GetComponent<GUIText>().color = diffcolormap[list[index]];
 					}
@@ -325,9 +361,9 @@ public class MenuController : MonoBehaviour
 					Vector gestureVector = swipeGesture.Direction;
 					float x = gestureVector.x;
 					float y = gestureVector.y;
-					if (x < - 0.7f && (menulist [0].transform.position.x + (menulist.Count - 1) * 0.5) > 0.6f) {
+					if (x < - 0.7f && (menulist [0].transform.position.x + (menulist.Count - 1) * 0.5) > spaceHorizontal * 0.5f) {
 						lastmotion = -1;
-					} else if (x > 0.7f && menulist [0].transform.position.x < 0.4f) {
+					} else if (x > 0.7f && menulist [0].transform.position.x < -spaceHorizontal * 0.5f) {
 						lastmotion = 1;
 					} else if (y < -0.7f) { 
 						// change difficult
@@ -335,14 +371,14 @@ public class MenuController : MonoBehaviour
 						motionlock = true;
 						foreach (var item in menulist) {
 							Vector3 position = item.transform.position;
-							if (position.x > 0.4 && position.x < 0.6) {
-								var list = difflist [item.GetComponent<GUIText> ().text];
-								int index = list.IndexOf (item.transform.GetChild (0).GetComponent<GUIText> ().text);
+							if (position.x > -spaceHorizontal * 0.5f && position.x < spaceHorizontal * 0.5f) {
+								var list = difflist [item.transform.GetChild(0).GetComponent<Text> ().text];
+								int index = list.IndexOf (item.transform.GetChild (1).GetComponent<Text> ().text);
 								Debug.Log ("diff content list " + list.ToString () + " index: " + index);
 								
 								index = (index + 1) % list.Count;
 								difftonext = list [index];
-								difftextobj = item.transform.GetChild (0).GetComponent<GUIText> ();
+								difftextobj = item.transform.GetChild (1).GetComponent<Text> ();
 								//item.transform.GetChild(0).GetComponent<GUIText>().text = list[index];
 								//item.transform.GetChild(0).GetComponent<GUIText>().color = diffcolormap[list[index]];
 							}
@@ -361,4 +397,16 @@ public class MenuController : MonoBehaviour
 			}
 		}
 	}
+
+    IEnumerator LoadMusic(GameObject item, string folder, string name)
+    {
+        var music = GetComponent<AudioSource>();
+        ResourceRequest request = Resources.LoadAsync("Music/" + folder + "/" + name);
+        yield return request;
+        music.clip = request.asset as AudioClip;
+        Debug.Log("paly music demo " + "/Music/" + folder + "/" + name);
+        music.time = this.songstart[item.transform.GetChild(0).GetComponent<Text>().text];
+        music.loop = true;
+        music.Play();
+    }
 }
